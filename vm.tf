bdbae8fe79db87cbe151cf9f268584e21810c88e
Keer-ops <keerthanakk321@gmail.com>
2026-05-11 17:21:18 +0530

    Update_vm.tf

--- vm.tf
+++ vm.tf
@@ -3,7 +3,7 @@ resource "azurerm_linux_virtual_machine" "frontend_vm" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
-  size = "Standard_B2als_v2"
+  size = "Standard_B1as_v2"
 
   admin_username = "azureuser"
 
@@ -34,7 +34,7 @@ resource "azurerm_linux_virtual_machine" "backend_vm" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
-  size = "Standard_B2als_v2"
+  size = "Standard_B1as_v2"
 
   admin_username = "azureuser"
 
