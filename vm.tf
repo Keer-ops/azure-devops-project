9bf23bc69da544c083c91d8619a60bcfbfa5cbba
keer <keerthanakk321@gmail.com>
2026-05-11 14:55:43 +0530

    change vm size to b1s

--- vm.tf
+++ vm.tf
@@ -3,7 +3,7 @@ resource "azurerm_linux_virtual_machine" "frontend_vm" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
-  size = "Standard_A1_v2"
+  size = "Standard_B1s"
 
   admin_username = "azureuser"
 
@@ -34,7 +34,7 @@ resource "azurerm_linux_virtual_machine" "backend_vm" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
-  size = "Standard_A1_v2"
+  size = "Standard_B1s"
 
   admin_username = "azureuser"
 
